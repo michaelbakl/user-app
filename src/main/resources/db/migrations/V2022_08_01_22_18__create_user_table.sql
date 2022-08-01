090e930ef0296d0a450648762fe1ba220c32f1db
Michael Baklykov <baklykovmichael@gmail.com>
2022-08-02 00:12:51 +0600

    [+] application.yml added, migrations for database added

---
+++ src/main/resources/db/migrations/V2022_08_01_22_18__create_user_table.sql
@@ -0,0 +1,5 @@
+create table if not exists "user"
+(
+    id text not null PRIMARY KEY,
+    fullName text not null
+);
---
+++ src/main/resources/db/migrations/V2022_08_01_22_21__fill_user_table.sql
@@ -0,0 +1,2 @@
+insert into "user"
+values ('1986b662-d6d7-11ec-9d64-0242ac120002', 'Mikhailov Mikhail Mikhailovich');
